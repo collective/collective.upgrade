27f7f1df4adb8d5efdfc9aa314867b65e87c2a07
Ross Patterson <me@rpatterson.net>
2012-02-26 16:16:13 -0800

    Bring in a bunch of generally useful upgrade steps and helpers:
    - delete custom skin objects
    - cleanup broken OFS objects
    - cleanup broken TextIndexes objects
    - cleanup broken component registrations
    - cleanup broken setup registrations
    - cleanup broken cmfeditions versions
    - migrate cmfeditions folder versions
    - cleanup duplicate UIDs
    - uninstall add-on helper
    - set default editor for all users

---
+++ src/collective/upgrade/broken.py
@@ -0,0 +1,170 @@
+from zope import interface
+from zope import component
+
+from ZODB import broken
+from ZODB import interfaces as zodb_ifaces
+
+from Acquisition import aq_base, aq_parent
+from Products.ZCatalog import ZCatalog
+
+from Products.GenericSetup import utils
+from Products.CMFCore.utils import getToolByName
+
+from collective.upgrade import steps
+
+
+def cleanupBrokenComponents(context):
+    """Delete component registrations from missing add-ons."""
+    portal = getToolByName(context, 'portal_url').getPortalObject()
+    sm = component.getSiteManager(context=portal)
+    for reg in list(sm.registeredUtilities()):
+        if not isinstance(reg.component, broken.Broken):
+            continue
+
+        steps.logger.info('Unregistering broken utility %r in %r' %
+                          (reg, portal))
+        sm.unregisterUtility(
+            component=reg.component, provided=reg.provided,
+            name=reg.name)
+
+def cleanupBrokenSetupRegistrations(context):
+    """Delete portal_setup registrations from missing add-ons."""
+    setup = getToolByName(context, 'portal_setup')
+    required = setup.getToolsetRegistry()._required
+    for id_, tool in required.items():
+        if utils._resolveDottedName(tool['class']) is None:
+            steps.logger.info(
+                'Unregistering missing required tool %r in %r' % (tool, setup))
+            required.pop(id_)
+            setup._p_changed = True
+
+    for profile_id in setup.listProfiles():
+        # Update a broken import step depdendency in the
+        # persisent registry which has been fixed in the
+        # profile import_steps.xml
+        steps.logger.info(
+            'Applying profile context %r in %r' % (profile_id, setup))
+        setup.applyContextById(profile_id)
+
+
+class Empty(object):
+    pass
+
+
+def cleanupBrokenTextIndexes(context):
+    catalog = getToolByName(context, 'portal_catalog')
+    for id_ in catalog.indexes():
+        index = catalog._catalog.getIndex(id_)
+        if (not isinstance(index, broken.Broken) or
+            'TextIndex' not in index.__class__.__name__):
+            continue
+
+        steps.logger.info('Replacing %r text index in %r' % (index, catalog))
+        catalog.manage_delIndex(id_)
+        extras = Empty()
+        extras.index_type = 'Okapi BM25 Rank'
+        extras.lexicon_id = 'plone_lexicon'
+        catalog.addIndex(id_, 'ZCTextIndex', extras)
+
+
+class CleanupBrokenObjects(steps.CMFEditionsUpgrader):
+
+    catalog_ids = ('portal_catalog', 'uid_catalog', 'reference_catalog')
+    update_catalogs = True
+
+    def upgrade(self):
+        self.upgradeObjects()
+
+        # Cleanup broken CMFEditions versions
+        super(CleanupBrokenObjects, self).upgrade()
+
+    def upgradeObjects(self):
+        # Clear the catalogs before walking the objects.  Objects will
+        # be indexed in the catalogs after being cleaned.
+        for zcatalog_id in self.catalog_ids:
+            zcatalog = getToolByName(self.context, zcatalog_id, None)
+            if zcatalog is not None:
+                self.log('Clearing the %r catalog' % zcatalog)
+                zcatalog.manage_catalogClear()
+            else:
+                self.log(
+                    'Could not find the %r catalog, skipping.' % zcatalog_id)
+
+        # Some broken objects may be transforms which require special handling
+        self.trans_tool = getToolByName(
+            self.context, 'portal_transforms', None)
+
+        # Walk all the portal objects fixing problems which might
+        # stop the upgrades.
+        self.log('Begin cleaning up objects in %r' % self.context)
+        self.context.ZopeFindAndApply(
+            self.context, search_sub=1, apply_func=self.upgradeObj)
+
+
+        # There should be no more broken interfaces
+        catalog = getToolByName(self.context, 'portal_catalog')
+        assert len(catalog(
+            object_provides=zodb_ifaces.IBroken.__identifier__)) == 0
+
+    def upgradeObj(self, obj, path=None):
+        obj._p_activate()
+
+        if self.delRemovedObj(obj, path):
+            # Deleted don't do anything else
+            return
+
+        if '__implements__' in obj.__dict__:
+            self.log('Deleting __implements__ instance attributre from %r'
+                     % obj)
+            del obj.__dict__['__implements__']
+            obj._p_changed = True
+
+        if zodb_ifaces.IBroken.providedBy(obj):
+            self.log('Removing broken interfaces from %r: %r' %
+                     (obj, list(interface.directlyProvidedBy(obj))))
+            interface.noLongerProvides(obj, zodb_ifaces.IBroken)
+
+        if not self.update_catalogs:
+            return
+        base = aq_base(obj)
+        
+        if (not isinstance(obj, ZCatalog.ZCatalog) and
+            callable(getattr(base, 'indexObject', None))):
+            obj.indexObject()
+
+        if callable(getattr(base, '_updateCatalog', None)):
+            obj._updateCatalog(obj)
+
+    def delRemovedObj(self, obj, path=None):
+        """Delete broken objects."""
+        if not isinstance(obj, broken.Broken):
+            return
+
+        if path is None:
+            path = '/'.join(obj.getPhysicalPath())
+        obj_id = path.rsplit('/', 1)[-1]
+        container = aq_parent(obj)
+
+        if aq_base(container) is aq_base(self.trans_tool):
+            self.log('Unregistering broken transform: %s' % path)
+            self.unmapTransform(container, obj)
+
+        self.log('Deleting broken object: %s' %  path)
+        container.manage_delObjects([obj_id])
+        assert container._getOb(obj_id, None) is None
+
+        return obj_id
+
+    def unmapTransform(self, container, transform):
+        """unmap transform from portal_transforms structures"""
+        for dest in container._mtmap.itervalues():
+            for transforms in dest.itervalues():
+                for registered in transforms:
+                    if aq_base(registered) is aq_base(transform):
+                        transforms.remove(transform)
+                        container._p_changed = True
+
+
+def cleanupBrokenObjects(context):
+    upgrader = CleanupBrokenObjects(context)
+    upgrader.upgrade()
--- src/collective/upgrade/steps.py
+++ src/collective/upgrade/steps.py
@@ -1,8 +1,14 @@
 import logging
+import re
+
+from Acquisition import aq_base
+from Products.PluginIndexes.FieldIndex import FieldIndex
 
 from Products.CMFCore.utils import getToolByName
 
-logger = logging.getLogger('collective.upgrade')
+from collective.upgrade import utils
+
+logger = logging.getLogger('collective.upgrade.steps')
 
 
 def deleteCustomSkinObjs(context, *obj_ids):
@@ -16,3 +22,155 @@ def deleteCustomSkinObjs(context, *obj_ids):
     logger.info(
         'Deleted custom skin objects from %r: %r'
         % (skins.custom, del_ids))
+
+
+def uninstallAddOns(context, addons=None):
+    """
+    Uninstall the given add-ons using the cleanest method possible.
+
+    If no add-ons are specified then all missing add-ons are
+    uninstalled.
+    """
+    qi = getToolByName(context, 'portal_quickinstaller')
+    for product in qi.listInstalledProducts():
+        addon = product['id']
+        if addons is not None and addon not in addons:
+            continue
+        elif qi.isProductInstallable(addon):
+            continue
+        
+        qi._getOb(addon).locked = False
+        uninstall_profiles = [
+            profile for profile in qi.getInstallProfiles(addon)
+            if profile.split(':', 1) == 'uninstall']
+        if uninstall_profiles:
+            profile = uninstall_profiles[0]
+            setup = getToolByName(context, 'portal_setup')
+            logger.info(
+                'Uninstalling the %r add-on for %r using the %r profile'
+                % (addon, qi, profile))
+            setup.runAllImportStepsFromProfile('profile-%s' % profile)
+            qi.manage_delObjects([addon])
+        else:
+            qi.uninstallProducts([addon])
+        assert not qi.isProductInstalled(addon)
+
+
+class CMFEditionsUpgrader(utils.Upgrader):
+
+    def upgrade(self):
+        self.storage = getToolByName(
+            self.context, 'portal_historiesstorage', None)
+        if self.storage is None:
+            self.log('No portal_historiesstorage tool found, '
+                     'skipping CMFEditions cleanup')
+            return
+
+        from Products.CMFEditions.interfaces.IArchivist import IVersionAwareReference
+        self.reference_iface = IVersionAwareReference
+
+        for obj in self.walkVersionObjects():
+            self.recurse(obj)
+
+    def walkVersionObjects(self):
+        repo = self.storage._getZVCRepo()
+        self.log('Begin cleaning up CMFEditions objects in %r' % self.storage)
+        for history_id in repo._histories:
+            history = repo.getVersionHistory(history_id)
+            self.log('Processing history %r' % history)
+            for branch in history.objectValues():
+                for version_id in branch.versionIds():
+                    version = history.getVersionById(version_id)
+                    obj = version._data.getWrappedObject().object
+                    self.recurse(obj)
+
+    def recurse(self, obj):
+        self.upgradeObj(obj)
+        if hasattr(aq_base(obj), 'objectValues'):
+            for subobj in obj.objectValues():
+                if self.reference_iface.providedBy(subobj):
+                    continue
+                self.recurse(subobj)
+
+
+class CMFEditionsFolderMigrator(CMFEditionsUpgrader):
+
+    def upgrade(self):
+        from Products.BTreeFolder2.BTreeFolder2 import BTreeFolder2Base as BTreeFolder
+        from plone.app.folder.migration import BTreeMigrationView
+        self.folder_class = BTreeFolder
+        self.folder_migrator = BTreeMigrationView(self.context, None)
+
+        super(CMFEditionsFolderMigrator, self).upgrade()
+
+    def upgradeObj(self, obj):
+        if isinstance(obj, self.folder_class):
+            self.folder_migrator.migrate(obj)
+
+
+def migrateCMFEditionsFolderVersions(context):
+    upgrader = CMFEditionsFolderMigrator(context)
+    upgrader.upgrade()
+
+
+copy_id_re = re.compile(r'^copy[0-9]*_of_.*')
+
+def origKey(obj):
+    """
+    The object is assumed to be the original if:
+
+    - it was created first
+    - or it doesn't have an id starting with 'copy_of_'
+    - or it has the shortest path
+    """
+    return (obj.created(),
+            copy_id_re.match(obj.getId()),
+            len(obj.getPhysicalPath()))
+
+def fixDuplicateUIDs(context):
+    """
+    Fix duplidate UIDs.
+
+    Try to make an intelligent guess as to which object is the
+    original and should keep the UID.  Missing objects are removed
+    from the catalog and new UIDs are generated for the rest of the
+    objects with the same UID.
+    """
+    catalog = getToolByName(context, 'portal_catalog')
+    ref_catalog = getToolByName(context, 'reference_catalog')
+    uid_index = catalog.Indexes._getOb('UID', None)
+    if not isinstance(uid_index, FieldIndex.FieldIndex):
+        return
+    for uid, rids in uid_index._index.iteritems():
+        if isinstance(rids, int) or len(rids) <= 1:
+            continue
+
+        objs = []
+        for rid in rids:
+            path = catalog.getpath(rid)
+            obj = context.unrestrictedTraverse(path, None)
+            if obj is None:
+                logger.info(
+                    'Removing missing object %r from %r' % (path, catalog))
+                catalog.uncatalog_object(path)
+                continue
+            objs.append(obj)
+        if not objs:
+            continue
+            
+        orig = min(objs, key=origKey)
+        logger.info('Multiple objects found for UID %r, '
+                    'assuming %r is the original' % (uid, orig))
+        for obj in objs:
+            if aq_base(obj) is aq_base(orig):
+                continue
+            new_uid = ref_catalog._getUUIDFor(obj)
+            obj.reindexObject(idxs=['UID'])
+            logger.info('Assigned new UID %r to %r' % (new_uid, obj))
+
+
+def setDefaultEditor(context, wanted_editor='', dry_run=False):
+    """Use the deault editor for all users."""
+    from collective.setdefaulteditor.utils import set_editor_for_all
+    # Assumes the zope.component.hooks site has already been set
+    set_editor_for_all(wanted_editor, dry_run)
