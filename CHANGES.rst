84a371d25eb2d45524f97daef4a4eb0dfa5eeee3
Thomas Kastenholz <github@kronix.eu>
2022-03-01 09:31:19 +0100

    Postrelease

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,12 @@ Changelog
 =========
 
 
+1.8 (unreleased)
+----------------
+
+- Nothing changed yet.
+
+
 1.7 (2022-03-01)
 ----------------
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 from setuptools import setup, find_packages
 
-version = '1.7'
+version = '1.8.dev0'
 
 install_requires = [
     'setuptools',
